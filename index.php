2b631d2d6abeafa987a2aff62d285c322d95b504
mambax7 <mambax7@gmail.com>
2020-10-25 03:38:58 -0400

    initial
    Signed-off-by: mambax7 <mambax7@gmail.com>

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+
+    include 'header.php';
+    require XOOPS_ROOT_PATH . '/header.php';
+
+    include 'footer.php';
---
+++ xoops_version.php
@@ -0,0 +1,29 @@
+<?php
+// ------------------------------------------------------------------------- //
+//                  Module xentGen pour Xoops 2.0.7                     //
+//                              Version:  1.0                                //
+// ------------------------------------------------------------------------- //
+// Author: Milhouse                                        				     //
+// Purpose:                           				     //
+// email: hotkart@hotmail.com                                                //
+// URLs:                      												 //
+//---------------------------------------------------------------------------//
+global $xoopsModuleConfig;
+$modversion['name'] = 'xEntIntranet';
+$modversion['version'] = '1.0';
+$modversion['description'] = 'First page of the intranet';
+$modversion['credits'] = '';
+$modversion['author'] = 'Ecrit pour Xoops2<br>par Mathieu Delisle (M4d3L)';
+$modversion['license'] = '';
+$modversion['official'] = 1;
+$modversion['image'] = 'images/xent_intranet_logo.png';
+$modversion['help'] = '';
+$modversion['dirname'] = 'xentintranet';
+
+// Admin things
+$modversion['hasAdmin'] = 0;
+$modversion['adminindex'] = 'admin/index.php';
+$modversion['adminmenu'] = 'admin/menu.php';
+
+// Menu
+$modversion['hasMain'] = 1;
